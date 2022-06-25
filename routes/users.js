b786f7b637a0988265f0e610cec6f2b0cb62048e
jasonoh22 <jasonoh22@naver.com>
2022-06-25 14:40:47 +0900

    FEAT : 아이디 비번 닉네임 받기 #10

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,36 @@
 var express = require("express");
 var router = express.Router();
-
+var con = require("../database/db.js");
 /* GET users listing. */
 router.get("/", function (req, res, next) {
     res.send("respond with a resource");
 });
 
+router.post("/join", async function (req, res, next) {
+    var id = req.body.id;
+    var password = req.body.password;
+    var nickname = req.body.nickname;
+    var sql = "select id from user where id=?";
+    con.query(sql, id, function (err, data) {
+        if (data[0] == null) {
+            var sql2 =
+                "insert into user(id, password, nickname) values('" +
+                id +
+                "','" +
+                password +
+                "','" +
+                nickname +
+                "')";
+            con.query(sql2, [id, password, nickname], function (err, result) {
+                if (err) {
+                    throw res.json(err);
+                }
+                console.log("회원가입 성공");
+            });
+        } else {
+            res.json("exist ID try another ID");
+            console.log("exist ID try another ID");
+        }
+    });
+});
 module.exports = router;
