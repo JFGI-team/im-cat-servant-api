0fef2cf558a77ce0cf33bbba89de12017e3f943a
bubssi528 <intel1567@gmail.com>
2022-06-28 22:34:22 +0900

    REFACTOR : 파일이름에 date

--- controllers/upload.js
+++ controllers/upload.js
@@ -5,10 +5,13 @@ aws.config.loadFromPath(__dirname + "/../config/s3.json");
 module.exports.upload = function (req, res) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
+        myFileName = files.image.originalFilename // .jpg 분리하고 타임스탬프로 파일이름 중복시 구분
+            .split(".")
+            .join("-" + Date.now() + ".");
         var s3 = new aws.S3();
         var params = {
             Bucket: "imcat-image",
-            Key: new Date() + files.image.originalFilename,
+            Key: myFileName,
             ACL: "public-read",
             Body: require("fs").createReadStream(files.image.filepath),
             ContentType: "image/jpeg",
