373d471a499b8adb264d23154c74ccdca6b4ff8f
bubssi528 <intel1567@gmail.com>
2022-06-28 17:07:43 +0900

    Revert "FEAT : 이미지업로드 api 생성"
    This reverts commit a90ebbbb342352eb53aaa5b303b2d9def86477f0.

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const uploadRouter = require("./routes/upload");
 
 const app = express();
 
@@ -23,7 +22,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/upload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/upload.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const s3 = require("../controllers/upload");
-const testParam = require("../param/testParam");
-
-router.post("/", s3.upload);
-
-module.exports = router;
