099b30d5c0f00196235b430ad89938194f0adb4d
bubssi528 <intel1567@gmail.com>
2022-06-29 20:06:19 +0900

    REFACTOR: 변수 바꾸고 export 수정, 빈파일 체크#16
    validator로 하는게 너무 어려워서 파일 사이즈로 체크해서 에러 핸들링

--- controllers/upload.js
+++ controllers/upload.js
@@ -2,22 +2,27 @@ const formidable = require("formidable");
 const aws = require("aws-sdk");
 aws.config.loadFromPath(__dirname + "/../config/s3.json");
 
-module.exports.upload = function (req, res) {
-    var form = new formidable.IncomingForm();
+exports.upload = function (req, res) {
+    const form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-        myFileName = files.image.originalFilename // .jpg 분리하고 타임스탬프로 파일이름 중복시 구분
-            .split(".")
-            .join("-" + Date.now() + ".");
-        var s3 = new aws.S3();
-        var params = {
-            Bucket: "imcat-image",
-            Key: myFileName,
-            ACL: "public-read",
-            Body: require("fs").createReadStream(files.image.filepath),
-            ContentType: "image/jpeg",
-        };
-        s3.upload(params, function (err, data) {
-            res.json({ url: data.Location });
+        if (!files.image.size == 0) {
+            const myFileName = files.image.originalFilename
+                .split(".")
+                .join("-" + Date.now() + ".");
+            const s3 = new aws.S3();
+            const params = {
+                Bucket: "imcat-image",
+                Key: myFileName,
+                ACL: "public-read",
+                Body: require("fs").createReadStream(files.image.filepath),
+                ContentType: "image/jpeg",
+            };
+            s3.upload(params, function (err, data) {
+                res.json({ url: data.Location });
+            });
+        }
+        res.status(400).json({
+            msg: "error image is empty",
         });
     });
 };
