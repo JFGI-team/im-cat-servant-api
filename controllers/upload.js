85d21d67e1437cfe7eac1358a16b70d657de302c
bubssi528 <intel1567@gmail.com>
2022-06-30 19:34:23 +0900

    FEAT: 이미지 리사이즈 기능 추가 #20
    sharp는 설명은 간단하게 나와있는데 옵션에 대한 설명이 없어서 오래걸렸다... 빡친다

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,28 +1,29 @@
 const formidable = require("formidable");
 const aws = require("aws-sdk");
 aws.config.loadFromPath(__dirname + "/../config/s3.json");
+const sharp = require("sharp");
 
-exports.upload = function (req, res) {
+exports.upload = function (req, res, next) {
     const form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-        if (!files.image.size == 0) {
+        if (files.image.size == 0) {
+            res.status(400).json({ msg: "error: image is empty" });
+        } else {
+            const s3 = new aws.S3();
+            const body = sharp(files.image.filepath).resize(128, 128).jpeg();
             const myFileName = files.image.originalFilename
                 .split(".")
                 .join("-" + Date.now() + ".");
-            const s3 = new aws.S3();
             const params = {
                 Bucket: "imcat-image",
                 Key: myFileName,
                 ACL: "public-read",
-                Body: require("fs").createReadStream(files.image.filepath),
+                Body: body,
                 ContentType: "image/jpeg",
             };
             s3.upload(params, function (err, data) {
                 res.json({ url: data.Location });
             });
         }
-        res.status(400).json({
-            msg: "error image is empty",
-        });
     });
 };
