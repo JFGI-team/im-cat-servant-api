fa4ccea7d70a3bb47ee588befc845aec96dbb9d0
jasonoh22 <jasonoh22@naver.com>
2022-07-05 17:53:47 +0900

    REFACTOR: 아이디 비번 닉네임 받기 #10

--- models/modelUser.js
+++ models/modelUser.js
@@ -11,8 +11,7 @@ exports.getUserInfoById = async function (id) {
                 id = ?
             `;
         db.query(query, [id], function (err, data) {
-            resolve(data);
-            console.log(data);
+            resolve(data[0]);
         });
     });
 };
@@ -21,20 +20,38 @@ exports.insertUserAtJoin = async function (id, password, salt, nickname) {
     return new Promise(function (resolve, reject) {
         var query2 = `
                 INSERT INTO
-                    user(id, password, salt, nickname)
+                    user ( id, password, salt, nickname)
                 VALUES
-                    (? ,? ,?, ?)
+                    ( ?, ?, ?, ?)
                 `;
         db.query(
             query2,
             [id, password, salt, nickname],
             function (err, result) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log("회원가입 완료");
+                if (err) res.send(err);
+                else {
+                    res.send("회원가입 완료");
                 }
             },
         );
+    });
+};
+
+exports.findUserAtDb = async function (id) {
+    return new Promise(function (resolve, reject) {
+        var query3 = `
+            SELECT
+                id, password, salt, nickname
+            FROM
+                user
+            WHERE
+                id = ?
+            `;
+        db.query(query3, [id], function (err, result) {
+            if (err) res.send(err);
+            else {
+                resolve(result);
+            }
+        });
     });
 };
