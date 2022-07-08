18193882e6d06cd54236018d20c84ad7b3bd3f95
bubssi528 <intel1567@gmail.com>
2022-07-09 08:41:29 +0900

    STYLE: 테스트한 코드가 남아있어서 삭제했습니다

--- controllers/object.js
+++ controllers/object.js
@@ -1,15 +1,5 @@
 const object = require("../models/modelObject");
 
-exports.getTestObject = async function (req, res, next) {
-    const objectInfo = await object.getObjectById(1);
-    res.json({
-        testParam: req.body.testParam,
-        sampleParam: req.body.sampleParam,
-        objectId: objectInfo[0].object_id,
-        objectName: objectInfo[0].name,
-    });
-};
-
 exports.getColorAndDirection = async function (req, res, next) {
     const objectInfo = await object.getColorAndDirection(1);
     const values = Object.values(objectInfo[0]);
