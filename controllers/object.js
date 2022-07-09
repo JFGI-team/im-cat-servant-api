2512c1b23f3dd3d117a03435d2d22dabd16ad74d
bubssi528 <intel1567@gmail.com>
2022-07-09 19:17:26 +0900

    STYLE: console.log 삭제

--- controllers/object.js
+++ controllers/object.js
@@ -2,7 +2,6 @@ const object = require("../models/modelObject");
 
 exports.getColorAndDirection = async function (req, res, next) {
     const objectInfo = await object.getColorAndDirection(req.body.objectId);
-    console.log(objectInfo);
 
     res.json({
         objectId: objectInfo.object_id,
