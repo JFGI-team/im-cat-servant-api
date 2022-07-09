31977256edde2fbaf72c4468de98db0799fa3ce0
jasonoh22 <jasonoh22@naver.com>
2022-07-09 19:15:26 +0900

    REFACTOR: 응답 메시지들 수정

--- controllers/user.js
+++ controllers/user.js
@@ -9,14 +9,15 @@ exports.insertUser = async function (req, res, next) {
     );
     const userInfo = await user.getUserInfoById(req.body.id);
     if (!userInfo) {
-        user.insertUserAtJoin(
+        await user.insertUserAtJoin(
             req.body.id,
             hashedPasswordAndSalt.password,
             hashedPasswordAndSalt.salt,
             req.body.nickname,
         );
+        res.status(200).json({ message: "success" });
     } else {
-        res.status(400).json({ msg: "exist ID try another ID" });
+        res.status(400).json({ message: "exist ID try another ID" });
     }
 };
 exports.findUser = async function (req, res, next) {
@@ -27,20 +28,20 @@ exports.findUser = async function (req, res, next) {
         checkUserInfo[0].password,
     );
     if (!verified) {
-        res.status(400).json({ msg: "비밀번호가 일치하지 않습니다." });
+        res.status(400).json({ error: "비밀번호가 일치하지 않습니다." });
     } else {
         const token = await publishToken.createToken(
             req.body.id,
             req.body.nickname,
         );
-        return res.status(200).send({ token });
+        return res.status(200).json({ token });
     }
 };
 exports.checkToken = async function (req, res, next) {
     const checkToken = publishToken.verifyToken(req.headers.token);
     if (!checkToken) {
-        return res.status(200).send("Token valid");
+        return res.status(200).json({ message: "success" });
     } else {
-        return res.status(404).send("Token not valid");
+        return res.status(404).json({ error: "Token not valid" });
     }
 };
