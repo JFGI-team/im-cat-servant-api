929d75c68da917af406f741d3ae135d55424115f
bubssi528 <intel1567@gmail.com>
2022-07-22 18:40:19 +0900

    FEAT: 에러응답 추가 #41
    검색결과가 0이면 검색결과가 없음 에러메세지 전송 + 페이징리스트에 없는 lastMapId를 요청할 경우  유효하지 않는 id라고 에러메세지 전송 + start -> index 수정

--- controllers/object.js
+++ controllers/object.js
@@ -1,4 +1,5 @@
 const object = require("../models/modelObject");
+const objectCat = require("../models/modelObjectCat");
 
 exports.getColorAndDirection = async function (req, res, next) {
     const objectInfo = await object.getColorAndDirection(req.body.objectId);
@@ -15,23 +16,35 @@ exports.getObjectIdList = async function (req, res, next) {
         req.body.searchKeyword,
         req.body.category,
     );
-
     const objectListObj = new Object({
         totalCount: 0,
         objects: [],
-        start: 0,
+        index: 0,
         limit: req.body.limit,
     });
 
     if (objectIdStr.object_id) {
         objectListObj.objects = objectIdStr.object_id.split(",");
         objectListObj.totalCount = objectListObj.objects.length;
-        objectListObj.start =
-            objectListObj.objects.indexOf(String(req.body.lastMapId)) + 1;
-        objectListObj.objects = objectListObj.objects.slice(
-            objectListObj.start,
-            objectListObj.start + objectListObj.limit,
+        objectListObj.index = objectListObj.objects.indexOf(
+            String(req.body.lastMapId),
         );
+    } else {
+        return res.status(400).json({
+            error: "검색 결과가 존재하지 않습니다.",
+        });
+    }
+    if (!req.body.lastMapId || objectListObj.index !== -1) {
+        objectListObj.objects = objectListObj.objects.slice(
+            objectListObj.index + 1,
+            objectListObj.index + 1 + objectListObj.limit,
+        );
+    } else {
+        return res.status(400).json({
+            error: "유효하지 않는 lastMapId 값입니다.",
+        });
+    }
+    if (objectListObj.objects.length) {
         objectListObj.objects = await object.getObejctListByIdList(
             objectListObj.objects,
         );
@@ -40,17 +53,5 @@ exports.getObjectIdList = async function (req, res, next) {
     res.json({
         totalCount: objectListObj.totalCount,
         objects: objectListObj.objects,
-    });
-};
-
-exports.getRandomCat = async function (req, res, next) {
-    const catIdList = await cat.getCatIdList;
-    console.log(catIdList);
-    const randCatId = catIdList[(catIdList.length * Math.random()) | 0];
-    const randCatImage = await cat.getCatInfoById(randCatId);
-
-    res.json({
-        catId: randCatId,
-        imageUrl: randCatImage,
     });
 };
