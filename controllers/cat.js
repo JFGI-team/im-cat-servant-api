9dfe3c4126fdbdede4745b04776111dd2c481e77
bubssi528 <intel1567@gmail.com>
2022-08-01 17:20:18 +0900

    STYLE: 수정

--- controllers/cat.js
+++ controllers/cat.js
@@ -1,5 +1,4 @@
 const objectCat = require("../models/modelObjectCat");
-const catMapping = require("../models/modelMapCatMapping");
 
 exports.getRandomCat = async function (req, res, next) {
     const catMappingList = await objectCat.getCatMappingListByMapId(
@@ -21,15 +20,5 @@ exports.getRandomCat = async function (req, res, next) {
     res.json({
         catId: randomCat.object_cat_id,
         imageUrl: randomCat.image_url,
-    });
-};
-
-exports.setCatHead = async function (req, res, next) {
-    const catList = objectCat.getCatMappingListByMapId(req.body.mapId);
-    if (catList)
-        catMapping.setCatHeadByMapIdAndCatId(req.body.mapId, req.body.catId);
-
-    res.status(400).json({
-        message: "대표고양이 설정이 완료됐습니다.",
     });
 };
